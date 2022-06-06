d979314b5eb48b0ce43f8cf73e65c41367dabc1c
Nedyalko Dyakov <nedyalko.dyakov@gmail.com>
2022-06-06 15:58:10 +0300

    Introduce apiURL in sparkpost constructor.

--- sparkpost.go
+++ sparkpost.go
@@ -39,10 +39,11 @@ func (*SparkPostService) Serialize(m *Mail) ([]byte, error) {
 }
 
 // NewSparkPostClient creates a new client with a SparkPost API key.
-func NewSparkPostClient(apiKey string, tracers ...Tracer) Sender {
+// default api url "https://api.sparkpost.com/api/v1/transmissions?num_rcpt_errors=3"
+func NewSparkPostClient(apiKey string, apiURL string, tracers ...Tracer) Sender {
 	return &Client{
 		APIKey:  apiKey,
-		APIURL:  "https://api.sparkpost.com/api/v1/transmissions?num_rcpt_errors=3",
+		APIURL:  apiURL,
 		Service: new(SparkPostService),
 		Tracer:  composedTracer{tracers},
 	}
